e9581f104ec169e43aec308903c05dc4136fd080
ElMescalito92 <felix.harder92@gmail.com>
2021-02-02 11:53:14 +0100

    style: Removed comments for readability

--- Advanced_PW_Gen.py
+++ Advanced_PW_Gen.py
@@ -9,7 +9,6 @@ def prnt_hold(n, messages):
     time.sleep(n)
 
 
-# Start Generator/Welcome Screen
 def start_generator():
     pass_len = []
     character_select = []
@@ -18,14 +17,9 @@ def start_generator():
     password_length(character_select, pass_len, password)
 
 
-# PW-Length Function
 def password_length(character_select, pass_len, password):
     pass_len = input("\nPlease enter desired password length:\n")
     if int(pass_len) >= 12 and int(pass_len) <= 64:
-        # print(pass_len)
-        # Send value to Choose Character Function
-        # prnt_hold(1, "\nYour password will be " + pass_len + "
-        # characters long.")
         password_characters(character_select, pass_len, password)
     else:
         prnt_hold(2, "\nPassword needs to be at least 12 Characters "
@@ -34,9 +28,7 @@ def password_length(character_select, pass_len, password):
         password_length(character_select, pass_len, password)
 
 
-# Choose Characters Function
 def password_characters(character_select, pass_len, password):
-    # Choose
     prnt_hold(0, "\nPlease select the character contents for your Password.")
     characters = input("Enter 1: Alphabetical only. "
                        "(not recommended)\n"
@@ -45,7 +37,6 @@ def password_characters(character_select, pass_len, password):
                        "and punctuations. (Safest)\n")
     if int(characters) <= 3:
         character_select.append(characters)
-        # prnt_hold(1, "\nYou chose option " + characters + ".\n")
         create_password(character_select, pass_len, password)
     else:
         prnt_hold(2, "\nPlease enter number between 1 and 3.\n")
@@ -53,7 +44,6 @@ def password_characters(character_select, pass_len, password):
 
 
 def create_password(character_select, pass_len, password):
-    # password = ["\n"]
     if "3" in character_select:
         while len(password) < int(pass_len):
             password_character = random.choice(string.ascii_letters +
@@ -87,7 +77,6 @@ def password_string(character_select, pass_len, password):
 def repeat_generator(character_select, pass_len, password):
     restart_generator = input("\nGenerate again? (Y/N):\n").lower()
     if restart_generator == "y":
-        # create PW-function
         password_length(character_select, pass_len, password)
     elif restart_generator == "n":
         exit()
