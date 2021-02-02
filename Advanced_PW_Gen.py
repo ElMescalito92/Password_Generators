cbcd9c92e01a54b47806f94ddd468083eb24953f
ElMescalito92 <felix.harder92@gmail.com>
2021-02-02 11:24:11 +0100

    feat: Added Advanced_PW_Gen.py (Initial Commit)
    With the Advanced PW Generator, Users can input
    desired length of the password, and choose which
    characters will be used in the generated PW.

---
+++ Advanced_PW_Gen.py
@@ -0,0 +1,96 @@
+import random
+import time
+import string
+import sys
+
+
+def prnt_hold(n, messages):
+    print(messages)
+    time.sleep(n)
+
+
+#Start Generator/Welcome Screen
+def start_generator():
+    pass_len = []
+    character_select = []
+    password = [""]
+    prnt_hold(0, "\nWelcome!\n ")
+    password_length(character_select, pass_len, password)
+
+
+#PW-Length Function
+def password_length(character_select, pass_len, password):
+    pass_len = input("\nPlease enter desired password length:\n")
+    if int(pass_len) >= 12 and int(pass_len) <= 64:
+        #print(pass_len)
+        #Send value to Choose Character Function
+        #prnt_hold(1, "\nYour password will be " + pass_len + " characters long.")
+        password_characters(character_select, pass_len, password)
+    else:
+        prnt_hold(2, "\nPassword needs to be at least 12 Characters to be safe!\n"
+                       "Length is limited to 64 Characters at the most!\n")
+        password_length(character_select, pass_len, password)
+
+#Choose Characters Function
+def password_characters(character_select, pass_len, password):
+    #Choose
+    prnt_hold(0, "\nPlease select the character contents for your Password.")
+    characters = input("Enter 1: Alphabetical only. (not recommended)\n"
+                       "Enter 2: Alphabetical and numerical. (Safe)\n"
+                       "Enter 3: Alphabetical, numerical and punctuations. (Safest)\n")
+    if int(characters) <= 3:
+        character_select.append(characters)
+        #prnt_hold(1, "\nYou chose option " + characters + ".\n")
+        create_password(character_select, pass_len, password)
+    else:
+        prnt_hold(2, "\nPlease enter number between 1 and 3.\n")
+        password_characters(character_select, pass_len, password)
+
+
+def create_password(character_select, pass_len, password):
+    #password = ["\n"]
+    if "3" in character_select:
+        while len(password) < int(pass_len):
+            password_character = random.choice(string.ascii_letters + string.digits + string.punctuation)
+            password.append(password_character)
+    elif "2" in character_select:
+        while len(password) < int(pass_len):
+            password_character = random.choice(string.ascii_letters + string.digits)
+            password.append(password_character)
+    else:
+        while len(password) < int(pass_len):
+            password_character = random.choice(string.ascii_letters)
+            password.append(password_character)
+    password_string(character_select, pass_len, password)
+
+
+def password_string(character_select, pass_len, password):
+    password_string = ''.join(str(e) for e in password)
+    prnt_hold(0.4, "")
+    prnt_hold(0.4, "\n.")
+    prnt_hold(0.4, ". .")
+    prnt_hold(0.4, ". . .")
+    prnt_hold(0.4, "\nYour generated password:\n")
+    print(password_string)
+    prnt_hold(5, '')
+    repeat_generator(character_select, pass_len, password)
+
+
+def repeat_generator(character_select, pass_len, password):
+    restart_generator = input("\nGenerate again? (Y/N):\n").lower()
+    if restart_generator == "y":
+        # create PW-function
+        password_length(character_select, pass_len, password)
+    elif restart_generator == "n":
+        exit()
+    else:
+        prnt_hold(2, "\nPlease choose Y or N.")
+        repeat_generator(character_select, pass_len, password)
+
+
+def exit():
+    prnt_hold(1, "\nGoodbye!\n")
+    sys.exit()
+
+
+start_generator()
